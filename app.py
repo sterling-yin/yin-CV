6c08114fb91864c63bbd5582e30ed5fee1abb373
Sterling Yin <77092084+sterling-yin@users.noreply.github.com>
2024-05-25 01:02:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -216,22 +216,21 @@ st.write(
 """
 )
 
-import streamlit as st
-import graphviz
 
-graph = graphviz.Digraph()
-graph.edge('run', 'intr')
-graph.edge('intr', 'runbl')
-graph.edge('runbl', 'run')
-graph.edge('run', 'kernel')
-graph.edge('kernel', 'zombie')
-graph.edge('kernel', 'sleep')
-graph.edge('kernel', 'runmem')
-graph.edge('sleep', 'swap')
-graph.edge('swap', 'runswap')
-graph.edge('runswap', 'new')
-graph.edge('runswap', 'runmem')
-graph.edge('new', 'runmem')
-graph.edge('sleep', 'runmem')
-
-st.graphviz_chart(graph)
+st.graphviz_chart('''
+    digraph {
+        "run RUN" -> "intr"
+        intr -> runbl
+        runbl -> run
+        run -> kernel
+        kernel -> zombie
+        kernel -> sleep
+        kernel -> runmem
+        sleep -> swap
+        swap -> runswap
+        runswap -> new
+        runswap -> runmem
+        new -> runmem
+        sleep -> runmem
+    }
+''')
