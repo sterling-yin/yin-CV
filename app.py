f3eb9aa0e036b06a3390f95fc68c07222a1b3cd6
Sterling Yin <77092084+sterling-yin@users.noreply.github.com>
2024-05-26 09:10:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -221,3 +221,12 @@ lineage = Image.open(lineage)
 st.write('\n')
 st.subheader("Academic Lineage")
 st.image(lineage, channels="BGR")
+
+tab1, tab2 = st.tabs(["📈 Chart", "🗃 Data"])
+data = np.random.randn(10, 1)
+
+tab1.subheader("A tab with a chart")
+tab1.line_chart(data)
+
+tab2.subheader("A tab with the data")
+tab2.write(data)
