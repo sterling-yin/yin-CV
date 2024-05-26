9067fb5a883f77357b2dfa98ee1f6c9ae3a0f69b
Sterling Yin <77092084+sterling-yin@users.noreply.github.com>
2024-05-26 09:50:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ with col2:
     st.write(DESCRIPTION)
     st.write("📧", EMAIL)
 
-tab1, tab2, tab3, tab4, tab5 = st.tabs(["🌟 Introduction", "🎖️ Featured Publications", "📑 Complete Publications List", "🏛️ Academic Lineage", "🗄️ Codes"])
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["🌟Introduction", "🎖️Featured Publications", "📑Publications List", "🏛️Academic Lineage", "🗄️Codes"])
 
 with tab1:
 # --- Biography ---
@@ -218,7 +218,7 @@ with tab5:
     """
 The ultra-high tensile capacity of strain-hardening fibre-reinforced cementitious composites makes them promising for impact dynamics applications, however, there is a lack of reference for taking the parameters of the constitutive model of materials in numerical simulations. In this study, the Karagozian & Case concrete/cementitious model parameters for strain-hardening fibre-reinforced cementitious composites were comprehensively and systematically calibrated. Multiaxial behaviour, crack-bridging constitutive relations, strain-rate effects, regularization of damage evolution parameters, and parameters scaling method for wide-range strength of materials were all considered and carefully discussed. The calibrated parameters were validated using low-velocity drop-weight impact tests, near-field explosion tests, and high-speed penetration tests. In this study, the traditional element erosion method and meshless/particle methods of Smoothed Particle Galerkin (SPG) method were used for failure analysis in penetration and explosion models. The results show that the calibrated parameters accurately predict the cratering and scabbing diameters and failure modes of explosion tests with the element erosion method. Furthermore, the residual velocity of the projectile and crack patterns of targets were also predicted with good precision with both element erosion and SPG methods. However, in the simulations of low-velocity impact tests, the predictions of maximum deflection were acceptable, and the errors in residual deflection were significant due to the inherent weakness of the material constitutive model.
 
-Please Cite: *Yin et al. (2023)*
+Please Cite: **Yin et al. (2023)**
 
 X. Yin, Q. Li, B. Chen, S. Xu, An improved calibration of Karagozian & Case concrete/cementitious model for strain-hardening fibre-reinforced cementitious composites under explosion and penetration loadings, Cem. Concr. Compos. 137 (2023) 104911.
     """
@@ -421,4 +421,4 @@ $     BulkUnld06      BulkUnld07      BulkUnld08      BulkUnld09      BulkUnld10
 $#            k6              k7              k8              k9             k10
 &k6             &k7             &k8             &k9             &k10            
 *END'''
-   st.code(code, language='fortran')
+   st.code(code, language=None, line_numbers=True)
