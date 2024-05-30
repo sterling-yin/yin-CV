f9ee0bf5ec779c7b55d222c4c52a2d9f36477e30
Sterling Yin <77092084+sterling-yin@users.noreply.github.com>
2024-05-30 10:12:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ profile_pic = Image.open(profile_pic)
 # --- HERO SECTION ---
 col1, col2 = st.columns(2, gap="small")
 with col1:
-    st.image(profile_pic, width=300)
+    st.image(profile_pic, width=320)
 
 with col2:
     st.title(NAME)
