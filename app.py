13e69c43b2c281b691b5d905360257560e10272f
Sterling Yin <77092084+sterling-yin@users.noreply.github.com>
2024-06-11 13:20:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 
 # --- LOAD CSS, PDF & PROFIL PIC ---
 with open(css_file) as f:
-    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+    st.markdown("<style>{}</style>".format(f.read()), "<div style='text-align: justify'>", unsafe_allow_html=True)
 with open(resume_file, 'rb') as pdf_file:
     PDFbyte = pdf_file.read()
 profile_pic = Image.open(profile_pic)
