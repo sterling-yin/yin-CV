77f6b986550e04822cea92ee76596dd9e8742e97
Sterling Yin <77092084+sterling-yin@users.noreply.github.com>
2024-09-16 23:12:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -145,20 +145,21 @@ My research focuses on ***Impact Dynamics***, and I have authored/co-authored 17
 
 with tab2:
 # --- Selected Publications ---
-
-   Publications1 = {
-    "► An improved calibration of Karagozian & Case concrete/cementitious model for strain-hardening fibre-reinforced cementitious composites under explosion and penetration loadings. Cement and Concrete Composites. 2023;137:104911.": "https://www.doi.org/10.1016/j.cemconcomp.2022.104911",
-    "► Investigation of continuous surface cap model (CSCM) for numerical simulation of strain-hardening fibre-reinforced cementitious composites against low-velocity impacts. Composite Structures. 2023;304:116424.": "https://www.doi.org/10.1016/j.compstruct.2022.116424",
-    "► Mesoscale numerical investigation of dynamic spalling fracture in toughness concrete. International Journal of Mechanical Sciences. 2024;264:108826.": "https://www.doi.org/10.1016/j.ijmecsci.2023.108826",
-    "► Near range explosion resistance of UHPFRC panels in wide scaled distances: Experimental study and stochastic numerical modelling. International Journal of Impact Engineering. 2024;192:105028": "https://www.doi.org/10.1016/j.ijimpeng.2024.105028",
-    "► Experimental and numerical investigations on the stress waves propagation in strain-hardening fiber-reinforced cementitious composites: Stochastic analysis using polynomial chaos expansions. Journal of Building Engineering. 2023;74:106902.": "https://www.doi.org/10.1016/j.jobe.2023.106902",
-    "► The double-K fracture model: A state-of-the-art review. Engineering Fracture Mechanics. 2023;277:108988.": "https://www.doi.org/10.1016/j.engfracmech.2022.108988",
-}
-
-   st.write('\n')
    st.subheader('🌌 Selected Publications')
-   for article, link in Publications1.items():
-       st.write(f"[{article}]({link})")
+
+#   Publications1 = {
+#    "► An improved calibration of Karagozian & Case concrete/cementitious model for strain-hardening fibre-reinforced cementitious composites under explosion and penetration loadings. Cement and Concrete Composites. 2023;137:104911.": "https://www.doi.org/10.1016/j.cemconcomp.2022.104911",
+#    "► Investigation of continuous surface cap model (CSCM) for numerical simulation of strain-hardening fibre-reinforced cementitious composites against low-velocity impacts. Composite Structures. 2023;304:116424.": "https://www.doi.org/10.1016/j.compstruct.2022.116424",
+#    "► Mesoscale numerical investigation of dynamic spalling fracture in toughness concrete. International Journal of Mechanical Sciences. 2024;264:108826.": "https://www.doi.org/10.1016/j.ijmecsci.2023.108826",
+#    "► Near range explosion resistance of UHPFRC panels in wide scaled distances: Experimental study and stochastic numerical modelling. International Journal of Impact Engineering. 2024;192:105028": "https://www.doi.org/10.1016/j.ijimpeng.2024.105028",
+#    "► Experimental and numerical investigations on the stress waves propagation in strain-hardening fiber-reinforced cementitious composites: Stochastic analysis using polynomial chaos expansions. Journal of Building Engineering. 2023;74:106902.": "https://www.doi.org/10.1016/j.jobe.2023.106902",
+#    "► The double-K fracture model: A state-of-the-art review. Engineering Fracture Mechanics. 2023;277:108988.": "https://www.doi.org/10.1016/j.engfracmech.2022.108988",
+#}
+
+#   st.write('\n')
+#   st.subheader('🌌 Selected Publications')
+#   for article, link in Publications1.items():
+#       st.write(f"[{article}]({link})")
 
 with tab3:
 # --- Complete List of Publications ---
